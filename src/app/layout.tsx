17d6dab4710beb8793b4f98e7f2ab041f0fb96be
melhipass <mel@hipassdesign.com>
2026-07-15 18:03:42 -0600

    Fixed errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <div id="root">{children}</div>
       </body>
     </html>
